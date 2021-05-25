15d97914e851ddc5231ffb9d1f01e1f72f120698
Axel Magnuson <axelmagn@google.com>
2021-05-25 00:21:37 -0700

    basic setup.py

--- mlops_manager/templates/app/setup.py
+++ mlops_manager/templates/app/setup.py
@@ -1,1 +1,20 @@
-# TODO(axelmagn)
+import setuptools
+import datetime
+
+PACKAGE_NAME = "{{python_package_name}}"
+REQUIRED_PACKAGES = []
+VERSION = "HEAD"  # TODO(axelmagn): determine dynamically
+
+
+def setup():
+    setuptools.setup(
+        name=PACKAGE_NAME,
+        version=VERSION,
+        install_requires=REQUIRED_PACKAGES,
+        packages=setuptools.find_packages(),
+        # TODO(axelmagn): package data
+    )
+
+
+if __name__ == "__main__":
+    setup()
