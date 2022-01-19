512191d2e83005a06d0fd581e70dddfc9de54256
Axel Magnuson <axelmagn@google.com>
2022-01-18 16:49:57 -0800

    WIP add more example code to pipeline

--- mlops_manager/templates/pipeline/kfp-simple/__APP_NAME__/__APP_NAME__/pipelines/__PIPELINE_NAME__.py
+++ mlops_manager/templates/pipeline/kfp-simple/__APP_NAME__/__APP_NAME__/pipelines/__PIPELINE_NAME__.py
@@ -1,8 +1,9 @@
 import kfp
 import argparse
 from kfp.v2 import compiler
-import google.cloud.aiplatform as aip
 from typing import Dict, Any
+from google.cloud import aiplatform
+from google_cloud_pipeline_components import aiplatform as gcc_aip
 
 PIPELINE_NAME = "{{pipeline_name}}"
 
@@ -18,6 +19,51 @@ def pipeline(project_id: str):
     # https://www.kubeflow.org/docs/components/pipelines/sdk/build-pipeline/
     pass
 
+    # The first step of your workflow is a dataset generator.
+    # This step takes a Google Cloud pipeline component, providing the necessary
+    # input arguments, and uses the Python variable `ds_op` to define its
+    # output. Note that here the `ds_op` only stores the definition of the
+    # output but not the actual returned object from the execution. The value
+    # of the object is not accessible at the dsl.pipeline level, and can only be
+    # retrieved by providing it as the input to a downstream component.
+    ds_op = gcc_aip.ImageDatasetCreateOp(
+        project=project_id,
+        display_name=f"{PIPELINE_NAME}_dataset_create",
+        gcs_source="gs://cloud-samples-data/vision/automl_classification/flowers/all_data_v2.csv",
+        import_schema_uri=aiplatform.schema.dataset.ioformat.image.single_label_classification,
+    )
+
+    # The second step is a model training component. It takes the dataset
+    # outputted from the first step, supplies it as an input argument to the
+    # component (see `dataset=ds_op.outputs["dataset"]`), and will put its
+    # outputs into `training_job_run_op`.
+    training_job_run_op = gcc_aip.AutoMLImageTrainingJobRunOp(
+        project=project_id,
+        display_name=f"{PIPELINE_NAME}_training",
+        prediction_type="classification",
+        model_type="CLOUD",
+        base_model=None,
+        dataset=ds_op.outputs["dataset"],
+        model_display_name="iris-classification-model-mbsdk",
+        training_fraction_split=0.6,
+        validation_fraction_split=0.2,
+        test_fraction_split=0.2,
+        budget_milli_node_hours=8000,
+    )
+
+    # The third and fourth step are for deploying the model.
+    create_endpoint_op = gcc_aip.EndpointCreateOp(
+        project=project_id,
+        display_name=f"{PIPELINE_NAME}_create_endpoint",
+    )
+
+    model_deploy_op = gcc_aip.ModelDeployOp(
+        model=training_job_run_op.outputs["model"],
+        endpoint=create_endpoint_op.outputs['endpoint'],
+        automatic_resources_min_replica_count=1,
+        automatic_resources_max_replica_count=1,
+    )
+
 
 def compile(package_path: str):
     compiler.Compiler().compile(
@@ -31,7 +77,7 @@ def run_job(
     pipeline_root: str,
     pipeline_params: Dict[str, Any] = {},
 ):
-    job = aip.PipelineJob(
+    job = aiplatform.PipelineJob(
         display_name=PIPELINE_NAME,
         template_path=template_path,
         pipeline_root=pipeline_root,
@@ -71,7 +117,7 @@ def main(args):
     if args.command == "compile":
         compile(args.output)
     elif args.command == "run_job":
-        aip.init()
+        aiplatform.init()
         # If pipeline parameters are necessary, add them here.
         pipeline_params = {"project_id": args.project}
         run_job(args.package, args.pipeline_root,
