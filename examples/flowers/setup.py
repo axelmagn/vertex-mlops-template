a2e1af829b1acf97de23e2a4fcaf051f73b1f02b
Axel Magnuson <axelmagn@google.com>
2022-03-15 23:49:55 -0700

    WIP start new flowers app

---
+++ examples/flowers/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+REQUIRED_PACKAGES = []
+
+setup(
+    name='flowers',
+    version='1.0',
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(),
+    description='',  # TODO(author): description
+)
